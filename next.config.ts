f6964e29f5764e8c2a36234b4c8df656c7943c9f
Saloni-hub <pandeysaloni4july@gmail.com>
2025-02-23 16:25:58 +0530

    remove config

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://64.227.142.191:8080/application-test-v1.1';
-
-    return [
-      {
-        source: "/api/books", 
-        destination: `${apiUrl}/books`,
-      },
-      {
-        source: "/api/books/:id", 
-        destination: `${apiUrl}/books/:id`,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
