5611b6f9e18f2aa5b7a05552b56672161423a269
Saloni-hub <pandeysaloni4july@gmail.com>
2025-02-23 16:04:43 +0530

    updated config file

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   async rewrites() {
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
     return [
       {
-        source: "/api/books/:id",
-        destination: "http://64.227.142.191:8080/application-test-v1.1/books/:id",
+        source: "/api/books", 
+        destination: `${apiUrl}/books`,
       },
       {
-        source: "/api/books",
-        destination: "http://64.227.142.191:8080/application-test-v1.1/books",
+        source: "/api/books/:id", 
+        destination: `${apiUrl}/books/:id`,
       },
     ];
   },
